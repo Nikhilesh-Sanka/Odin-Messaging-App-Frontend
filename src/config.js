356dc1910bf8fca73498ef2b3926abb485c7d962
Nikhilesh-Sanka <sankanikhilesh2006.svc@gmail.com>
2024-10-08 16:22:03 +0530

    changing the server url

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const serverUrl = "https://odin-messenger.adaptable.app";
-export const serverUrl = "http://localhost:3000";
+export const serverUrl = "https://odin-messenger.adaptable.app";
+// export const serverUrl = "http://localhost:3000";
